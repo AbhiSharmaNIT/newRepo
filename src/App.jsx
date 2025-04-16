7bb0b7a2b10865c84b6dcd9d3fa0e63ae2b65dcf
Abhishek Kumar <096abhi222002@gmail.com>
2025-04-17 00:08:21 +0530

    Initial commit with all files

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <>
+      <div>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+          <h1 className="text-xl">IPO Web Application</h1>
+          <p>
+            This is a web application for IPO (Initial Public Offering)
+            management.
+          </p>
+          <p>Welcome to the IPO web application!</p>
+          <p>We are excited to have you here.</p>
+          <p>
+            This application will help you manage your IPO process efficiently.
+          </p>
+          <p>Stay tuned for more updates!</p>
+          <p>If you have any questions, feel free to reach out to us.</p>
+          <p>Thank you for using our application!</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
